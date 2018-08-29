7e768da4ea372b8b995d53b2fddb71f57735eef7
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-08-29 13:18:12 -0600

    Add attributes and area - end of Iteration 1

--- test/room_test.rb
+++ test/room_test.rb
@@ -5,8 +5,20 @@ require './lib/room'
 class RoomTest < Minitest::Test
 
   def test_it_exists
-    room = Room.new()
+    room = Room.new(:bedroom, 10, 13)
     assert_instance_of Room, room
+  end
+
+  def test_it_has_attributes
+    room = Room.new(:bedroom, 10, 13)
+    assert_equal :bedroom, room.category
+    assert_equal 10, room.length
+    assert_equal 13, room.width
+  end
+
+  def test_it_calculates_area
+    room = Room.new(:bedroom, 10, 13)
+    assert_equal 130, room.area
   end
 
 end
