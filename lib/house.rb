27a8478f038652b8f395d7b19af0c6102760162c
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-08-29 13:43:29 -0600

    add rooms_from_category

--- lib/house.rb
+++ lib/house.rb
@@ -13,4 +13,9 @@ class House
     @rooms << room
   end
 
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -30,4 +30,18 @@ class HouseTest < Minitest::Test
     assert_equal [room_1, room_2], house.rooms
   end
 
+  def test_it_find_all_rooms_of_specific_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add(room_1)
+    house.add(room_2)
+    house.add(room_3)
+    house.add(room_4)
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+    assert_equal [], house.rooms_from_category(:kitchen)
+  end
 end
